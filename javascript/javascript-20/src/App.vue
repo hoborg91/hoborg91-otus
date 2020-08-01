c5591d8ef42a8f7c5830726ac92541ade64b2a01
hoborg91 <hoborg91@gmail.com>
2020-08-01 13:11:02 +0300

    ДЗ к занятиям №19 и №20
    Структура приложения "Устный счет"
    В разделе Vue одна большая самостоятельная работа - SPA (Single Page Application) игра "Устный счет".
    Игра состоит из двух экранов - на первом экране пользователь выбирает настройки, которые будут использовать в игре - типы вычислений, сложность, время раунда.
    На этой же странице показывается статистика тренировок.
    Вторая страница - сама игра.
    Пользователь должен решить максимальное количество задач на заданное время.
    Мокапы - https://app.moqups.com/korzio@gmail.com/bTYyBLCtpU/edit/page/ad64222d5
    
    ---
    
    Подготовить общую структуру приложения - компоненты контейнеры для страниц приложения.
    Сделать первую страницу приложения - форму настроек.

---
+++ javascript/javascript-20/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <Settings 
+      :log="{ day: 24, solved: 10, total: 25, accuracy: 80, }"
+      :duration="7"
+      :difficulty="5"
+      :operations="[ 'addition', 'division', 'exponentiation', ]"
+      />
+    <Game 
+      :settings="{
+        duration: 7,
+        difficulty: 5,
+        operations: [ 'addition', 'division', 'exponentiation', ],
+      }"
+      />
+  </div>
+</template>
+
+<script>
+import Settings from './components/Settings.vue'
+import Game from './components/Game.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Settings,
+    Game,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ javascript/javascript-20/src/components/Settings.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1><img alt="Vue logo" src="./../assets/logo.png" width="20"> Привет!</h1>
+    <p>Добро пожаловать на {{log.day}} тренировочный день.</p>
+    <p>Ваш последний результат - решено {{log.solved}} из {{log.total}}.</p>
+    <p>Общая точность {{log.accuracy}}%.</p>
+    <label class="settings-line"><input v-model="duration" type="range" min="1" max="15" />Длительность {{duration}} мин.</label>
+    <label class="settings-line"><input v-model="difficulty" type="range" min="1" max="15" />Сложность {{difficulty}}</label>
+    <label class="settings-line"><input v-model="operations" type="checkbox" value="addition" />Сложение</label>
+    <label class="settings-line"><input v-model="operations" type="checkbox" value="subtraction" />Разность</label>
+    <label class="settings-line"><input v-model="operations" type="checkbox" value="multiplication" />Умножение</label>
+    <label class="settings-line"><input v-model="operations" type="checkbox" value="division" />Деление</label>
+    <label class="settings-line"><input v-model="operations" type="checkbox" value="exponentiation" />Возведение в степень</label>
+    <button>Играть</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Settings',
+  props: {
+    log: Object,
+    duration: Number,
+    difficulty: Number,
+    operations: Array,
+  }
+}
+</script>
+
+<style scoped>
+.settings-line {
+  display: block;
+  padding: 5px;
+}
+
+button {
+  margin: 5px;
+  border-radius: 5px;
+}
+</style>
